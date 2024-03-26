dbf215bef26f23fd0a3dc8d8b0206845cba37e78
KARTHIKA-id12 <147937445+KARTHIKA-id12@users.noreply.github.com>
2024-03-26 02:38:32 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,2 @@
 print{"this files defines the simple interest concept in this repo")
+In bussiness simple interest is very important and plays a major role in business world and also in finance management department
